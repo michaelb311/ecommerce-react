fb8b0accea541bef9482301194a857382aff78f4
Michael <78328898+michaelb311@users.noreply.github.com>
2022-09-13 09:16:35 -0500

    set up backend

---
+++ backend/data.js
@@ -0,0 +1,126 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p1.jpg',
+            price: 12,
+            countInStock: 2,
+            brand: 'McCook Woodworking',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '2',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p2.jpg',
+            price: 12,
+            countInStock: 12,
+            brand: 'McCook Woodworking',
+            rating: 4,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '3',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p3.jpg',
+            price: 12,
+            countInStock: 0,
+            brand: 'McCook Woodworking',
+            rating: 5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '4',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p4.jpg',
+            price: 12,
+            countInStock: 20,
+            brand: 'McCook Woodworking',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '5',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p5.jpg',
+            price: 12,
+            countInStock: 200,
+            brand: 'McCook Woodworking',
+            rating: 1,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '6',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p6.jpg',
+            price: 12,
+            countInStock: 32,
+            brand: 'McCook Woodworking',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '7',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p7.jpg',
+            price: 12,
+            countInStock: 23,
+            brand: 'McCook Woodworking',
+            rating: 3,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '8',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p8.jpg',
+            price: 12,
+            countInStock: 0,
+            brand: 'McCook Woodworking',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '9',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p9.jpg',
+            price: 12,
+            countInStock: 4,
+            brand: 'McCook Woodworking',
+            rating: 4,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+        {
+            _id: '10',
+            name: 'Premium Cutting Board',
+            category: 'Cutting Boards',
+            image: '../images/p10.jpg',
+            price: 12,
+            countInStock: 2,
+            brand: 'McCook Woodworking',
+            rating: 5,
+            numReviews: 10,
+            description: 'Beautiful, handcrafted, wood cutting board.'
+        },
+    ],
+}
+
+export default data;
